195ac02a83376a38f1ddd42b600e3fa887eedc83
nshvijay <vijaypalkumawat2020@gmail.com>
2025-12-24 02:37:47 +0530

    change

--- start.c
+++ start.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(){
     printf("hello my name is Vijay\n");
-    printf("so the coding journey officially starts");
+    printf("so the coding journey officially starts\n");
+    printf("%d\n",4+24);
 
 
  return 0;}
